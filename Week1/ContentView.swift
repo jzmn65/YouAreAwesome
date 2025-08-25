a3d80fde777e5732b88fee37c74bb09e8c1f0b66
Jazmine Singh <jzmn65@gmail.com>
2025-08-25 17:45:28 -0400

    Initial Commit

---
+++ Week1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Week1
+//
+//  Created by Jazmine Singh on 8/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week1/Week1App.swift
@@ -0,0 +1,17 @@
+//
+//  Week1App.swift
+//  Week1
+//
+//  Created by Jazmine Singh on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
